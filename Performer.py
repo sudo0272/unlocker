ace36a7187fced24acad55edcd3fc9507b702689
sudo180272 <sudo180272@protonmail.ch>
2021-06-20 19:40:31 +0900

    Performer: fix bug and change annotation

--- Performer.py
+++ Performer.py
@@ -1,5 +1,6 @@
 from PasswordProvider import PasswordProvider
 from typing import *
+import datetime
 
 class Performer:
     def __init__(self, password_providers: List[PasswordProvider]) -> None:
@@ -8,14 +9,17 @@ class Performer:
     def equip(self) -> None:
         pass
 
-    def unlock(self) -> bool:
+    def unlock(self) -> Tuple[bool, Union[str, None], datetime.timedelta]:
         pass
 
     def post_process(self) -> None:
         pass
 
-    def perform(self) -> List[Union[bool, None]]:
-        self.equip()
-        self.unlock()
-        self.post_process()
+    def perform(self) -> List[Union[Tuple[bool, Union[str, None], datetime.timedelta], None]]:
+        results: List[Union[Tuple[bool, Union[str, None], datetime.timedelta], None]] = []
+        results.append(self.equip())
+        results.append(self.unlock())
+        results.append(self.post_process())
+
+        return results
 
